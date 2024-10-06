c5902a70fbbd1a862cd055fb68bd00ec749cebbc
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-10-06 17:02:23 +0100

    changed home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,8 @@ export default async function Index() {
   return (
     <>
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars }
+      <main className="flex-1 flex justify-center flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4 text-center">Manage your life easier with the touch of a button</h2>
       </main>
     </>
   );
